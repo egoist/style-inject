fb96c19812ee93676dd80cdeccdb5978964e75f4
egoist <aprilorange.net@icloud.com>
2015-12-07 23:11:18 +0800

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = function appendStyle (css) {
+  css = css || '';
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+  if (style.styleSheet){
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+  head.appendChild(style);
+};
