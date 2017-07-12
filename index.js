e6c2c00ccd77095dfe88c7930674cea04783522e
Lee JeongHo <elasim@users.noreply.github.com>
2017-07-12 21:44:24 +0900

    bugfix: ie8 style rendering issue (#3)

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ module.exports = function styleInject (css, returnValue) {
   var head = document.head || document.getElementsByTagName('head')[0];
   var style = document.createElement('style');
   style.type = 'text/css';
+  head.appendChild(style);
+  
   if (style.styleSheet){
     style.styleSheet.cssText = css;
   } else {
     style.appendChild(document.createTextNode(css));
   }
-  head.appendChild(style);
   return returnValue;
 };
