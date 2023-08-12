edaef82f9b5b882196f4285db136104588d136ba
Ahmed Tarek <ahmed.tokyo1@gmail.com>
2023-08-12 13:58:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-export default function styleInject(css, { insertAt } = {}) {
+export default function styleInject(css, { insertAt, insertBeforeQuerySelector } = {}) {
   if (!css || typeof document === 'undefined') return
 
   const head = document.head || document.getElementsByTagName('head')[0]
+  const insertBeforeNode = insertBeforeQuerySelector && head.querySelector(insertBeforeQuerySelector);
   const style = document.createElement('style')
   style.type = 'text/css'
 
-  if (insertAt === 'top') {
+  // if an insertBeforeNode is found, insert before that node
+  if (insertBeforeNode) {
+    head.insertBefore(style, insertBeforeNode)
+  } else if (insertAt === 'top') {
+    // if an insertAt 'top' is passed, insert before head's first child
     if (head.firstChild) {
       head.insertBefore(style, head.firstChild)
     } else {
       head.appendChild(style)
     }
   } else {
+    // append child to head
     head.appendChild(style)
   }
 
